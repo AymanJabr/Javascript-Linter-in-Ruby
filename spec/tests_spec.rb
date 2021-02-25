fb33eea66ac38c6234f0f4993ba707eec07e72a4
ayman <aymanjaber2012@gmail.com>
2021-02-25 21:43:12 +0200

    Fix linter issues

--- spec/tests_spec.rb
+++ spec/tests_spec.rb
@@ -1,5 +1,5 @@
-require_relative '../lib/Syntax.rb'
-require_relative '../lib/Spacing.rb'
+require_relative '../lib/Syntax'
+require_relative '../lib/Spacing'
 
 spacing = Spacing.new
 syntax = Syntax.new
